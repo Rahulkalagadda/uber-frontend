05d641d1ba7a771d47a05a050f9342c18a93bee5
Rahulkalagadda <155637389+Rahulkalagadda@users.noreply.github.com>
2025-06-25 19:44:49 +0530

    Update socket.js

--- socket.js
+++ socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const socket = io("http://localhost:3000"); // Connect to backend port
+const socket = io("https://uber-backend-production-c74c.up.railway.app/"); // Connect to backend port
